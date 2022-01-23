e23857ec4664d852c67e4e8843928bdf4ecd2f30
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-22 23:05:02 -0500

    Adjust path of file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,15 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const form = document.getElementById("sign-in");
 
-function login(e) {
+function login(event) {
   //!!We aren't storing anything in a database yet, this is just a prototype. So as long as they type something in both fields, it signs in
-  e.preventDefault();
+  event.preventDefault();
   if (!emailInput.value){
     signupText.textContent = `Incorrect email!`;
   } else if (!passwordInput.value){
     signupText.textContent = `Incorrect password!`;
   } else {
-    location.assign('../pages/sign-in.html')
+    location.href='assets/pages/sign-in.html';
   }
 }
 
