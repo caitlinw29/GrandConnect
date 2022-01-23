cfd406344d5246b4728ae904ccfb190eb070834a
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-23 07:59:36 -0500

    Add sections for grandparents and kids forms, appear when they click the pertaining image

--- assets/js/signups.js
+++ assets/js/signups.js
@@ -1,12 +1,15 @@
 const seniorImg = document.getElementById('seniors');
 const kidsImg = document.getElementById('kids');
 const choicesMain = document.getElementById('sign-up-choices');
-
+const elderlySection = document.getElementById("elderlySection");
+const kidSection = document.getElementById("kidSection");
 
 seniorImg.addEventListener('click', function(){
   choicesMain.className = 'hidden';
+  elderlySection.classList.remove('hidden'); 
 })
 
 kidsImg.addEventListener('click', function(){
   choicesMain.className = 'hidden';
+  kidSection.classList.remove('hidden'); 
 })
